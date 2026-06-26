e98f6f815836ef09f37064dedbc69805eb7c4449
JoeyK09 <kinyanjuijoe099@gmail.com>
2026-06-26 15:30:05 +0300

    Modify database schema and add balance functions
    Updated user_id type to TEXT and added functions for balance retrieval and updating.

--- database.py
+++ database.py
@@ -1,13 +1,36 @@
 import sqlite3
 
-conn = sqlite3.connect("maveconnect.db", check_same_thread=False)
+conn = sqlite3.connect("plats.db", check_same_thread=False)
 cursor = conn.cursor()
 
 cursor.execute("""
 CREATE TABLE IF NOT EXISTS plats (
-    user_id INTEGER PRIMARY KEY,
+    user_id TEXT PRIMARY KEY,
     balance INTEGER DEFAULT 0
 )
 """)
-
 conn.commit()
+
+def get_balance(user_id):
+    cursor.execute(
+        "SELECT balance FROM plats WHERE user_id=?",
+        (user_id,)
+    )
+    row = cursor.fetchone()
+    return row[0] if row else 0
+
+def add_plats(user_id, amount):
+    balance = get_balance(user_id)
+
+    if balance == 0:
+        cursor.execute(
+            "INSERT OR IGNORE INTO plats(user_id, balance) VALUES(?, ?)",
+            (user_id, amount)
+        )
+    else:
+        cursor.execute(
+            "UPDATE plats SET balance=? WHERE user_id=?",
+            (balance + amount, user_id)
+        )
+
+    conn.commit()
