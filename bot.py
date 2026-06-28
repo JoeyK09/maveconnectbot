867cd07ad3ed9a7052baf8b4d4cf471acb707202
JoeyK09 <kinyanjuijoe099@gmail.com>
2026-06-28 22:09:49 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -269,6 +269,44 @@ COINPAPRIKA_IDS = {
     "dai": "dai-dai"
 }
 
+PICKAXES = {
+    1: {
+        "name": "🪵 Wooden",
+        "min": 10,
+        "max": 30,
+        "cooldown": 1800,
+        "price": 0
+    },
+    2: {
+        "name": "🪨 Stone",
+        "min": 20,
+        "max": 45,
+        "cooldown": 1680,
+        "price": 500
+    },
+    3: {
+        "name": "⛓ Iron",
+        "min": 35,
+        "max": 60,
+        "cooldown": 1500,
+        "price": 1500
+    },
+    4: {
+        "name": "🥇 Gold",
+        "min": 50,
+        "max": 90,
+        "cooldown": 1320,
+        "price": 4000
+    },
+    5: {
+        "name": "💎 Diamond",
+        "min": 80,
+        "max": 120,
+        "cooldown": 1200,
+        "price": 10000
+    }
+}
+
 def get_coin_id(symbol):
     if not symbol:
         return None
@@ -1178,14 +1216,6 @@ def do_mine(msg):
     balance, xp, level, pickaxe, last_daily, last_mine, wins = get_profile(user)
 
     now = int(time.time())
-
-    PICKAXES = {
-    1: {"min": 10, "max": 30, "cooldown": 1800},
-    2: {"min": 20, "max": 45, "cooldown": 1680},
-    3: {"min": 35, "max": 60, "cooldown": 1500},
-    4: {"min": 50, "max": 90, "cooldown": 1320},
-    5: {"min": 80, "max": 120, "cooldown": 1200},
-    }
 
     cooldown = PICKAXES[pickaxe]["cooldown"]
 
