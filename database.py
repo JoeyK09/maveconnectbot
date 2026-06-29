d5b5d84bc03d5bc76d27a1fa188c118ae1cd6ced
JoeyK09 <kinyanjuijoe099@gmail.com>
2026-06-29 09:34:11 +0300

    Update database.py

--- database.py
+++ database.py
@@ -1,13 +1,17 @@
 import os
 import psycopg2
+from config import PICKAXES
 
-PICKAXES = {
-    1: {"name": "🪵 Wooden", "min": 10, "max": 30, "cooldown": 1800, "price": 0},
-    2: {"name": "🪨 Stone", "min": 20, "max": 45, "cooldown": 1680, "price": 500},
-    3: {"name": "⛓ Iron", "min": 35, "max": 65, "cooldown": 1500, "price": 1500},
-    4: {"name": "🥇 Gold", "min": 55, "max": 90, "cooldown": 1320, "price": 4000},
-    5: {"name": "💎 Diamond", "min": 80, "max": 120, "cooldown": 1200, "price": 10000},
-}
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+if not DATABASE_URL:
+    raise Exception("DATABASE_URL not found")
+
+
+def get_connection():
+    conn = psycopg2.connect(DATABASE_URL)
+    conn.autocommit = True
+    return conn
 
 # ================= DATABASE CONNECTION =================
 
